0656d44547a909f009fd76b585cf5880bce7e779
queralt <queralt@mit.edu>
2018-11-14 12:28:35 +0100

    no changes

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.bibtex',
+    'sphinxcontrib.bibtex'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
